6089b474eed3153bf723b0027e1f5be31555deac
TriremeSolutions <themellowread@gmail.com>
2023-02-21 19:29:03 -0800

    env.c betty fix

--- env.c
+++ env.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
-int env(char **argv, char *envp[]){
-int ep;
+/**
+ * env - handles the input 'env'
+ * @argv: argument
+ * @envp: environment variables
+ * Return: 0
+ */
+int env(char **argv, char *envp[])
+{
+	int ep;
 	if (strcmp(argv[0], "env") == 0)
 	{
 		for (ep = 0; envp[ep] != NULL; ep++)
-		{
 			printf("\n%s\n", envp[ep]);
-		}
 	}
-return (0);
+	return (0);
 }
