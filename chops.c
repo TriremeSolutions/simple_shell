8225651e3422c535d7ef7959fe1cbe482d013ae7
TriremeSolutions <themellowread@gmail.com>
2023-02-21 19:56:18 -0800

    betty chops.c fixed

--- chops.c
+++ chops.c
@@ -1,10 +1,16 @@
 #include "main.h"
-
+/**
+ * chops - tokenizes user input
+ * @duplicate: copy of input string
+ * @lineBufPtr: user input string
+ * @delimiter: delimiter
+ * Return: void
+ */
 void chops(char *duplicate, char *lineBufPtr, const char *delimiter)
 {
-	int chopNumber;
-	char *chopped;
-	chopNumber = 0;
+int chopNumber;
+char *chopped;
+chopNumber = 0;
 	strcpy(duplicate, lineBufPtr);
 	chopped = strtok(lineBufPtr, delimiter); /*initial user input chopped*/
 	while (chopped != NULL)
