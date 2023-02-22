12a471b76ad8e7c5115ed1829d5bb97e2e476daa
TriremeSolutions <themellowread@gmail.com>
2023-02-21 19:34:44 -0800

    env.c

--- env.c
+++ env.c
@@ -8,7 +8,7 @@
 int env(char **argv, char *envp[])
 {
 	int ep;
-	if (strcmp(argv[0], "env") == 0)	
+	if (strcmp(argv[0], "env") == 0)
 	{
 		for (ep = 0; envp[ep] != NULL; ep++)
 			printf("\n%s\n", envp[ep]);
