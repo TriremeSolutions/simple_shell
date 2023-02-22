d664afa9534b7b043bd85a2fc4d1058c85cdbeea
TriremeSolutions <themellowread@gmail.com>
2023-02-21 19:15:11 -0800

    command.c betty fix

--- command.c
+++ command.c
@@ -1,79 +1,49 @@
 #include "main.h"
-
-int command(char **argv){
-    pid_t forkCheck;
-
-    /**
-    * initialize character strings to null values;
-    */
-
-    if (argv){
-        
-        
-        char *instructShell;
-        char *trueInstruct;
-        
-        if(strcmp(argv[0],"help")==0 || strcmp(argv[0],"yes")==0 || strcmp(argv[0],"hello")==0 ) {
-        	printf("Help manual is in development\n");
-        	return 0;
-        }
-        
-        if(strcmp(argv[0],"end")==0) {
-        	printf("use 'exit' to quit Madame $helby\n");
-        	return 0;
-        }
-        if(strcmp(argv[0],"shelby")==0) {
-        	printf("Listening!\n");
-        	return 0;
-        }
-
-        instructShell = argv[0];
-        
-        /**
-        * generate path for true instruction before execve() is called on it
-        */
-        trueInstruct = fetchPath(instructShell);
-
-        forkCheck = fork();
-
-        /**
-         * child, 0: Madame Shell instance
-         * parent 1: Bash shell
-         *
-         */
-        if (forkCheck == -1)
-        {
-                perror("Unsuccessul");
-                return 1;
-        }
-
-        if (forkCheck == 0)
-        {
-            /**
-             * printf("child process created\n");
-             */
-
-            if(execve(trueInstruct, argv, NULL) == -1){
-                perror("Error");
-                exit(0);
-            }
-        }
-
-        else
-        {
-            wait(NULL);
-            /**
-             * printf("arg %s\n",argv[0]);
-             * printf("parent process still on\n");
-             */
-
-            return (0);
-        }
-    }
-    /**
-     * end of if(argv)
-     * else is redundant
-     */
-
-    return (1);
+/**
+ * command - main loop of the shell
+ * @argv: input argument
+ *
+ * Return: an integer value ranging form 0 to 1
+ */
+int command(char **argv)
+{
+	pid_t forkCheck;if (argv)
+	{
+		char *instructShell, *trueInstruct;
+		
+		if (strcmp(argv[0], "help") == 0 || strcmp(argv[0], "yes") == 0)
+		{
+			printf("Help manual is in development\n");
+			return (0);
+		}
+		
+		if (strcmp(argv[0], "shelby") == 0)
+		{
+			printf("Listening!\n");
+			return (0);
+		}
+		instructShell = argv[0];
+		trueInstruct = fetchPath(instructShell);
+		forkCheck = fork();
+	
+		if (forkCheck == -1)
+		{
+			perror("Unsuccessul");
+			return (1);
+		}
+		if (forkCheck == 0)
+		{
+			if (execve(trueInstruct, argv, NULL) == -1)
+			{
+				perror("Error");
+				exit(0);
+			}
+		}
+		else
+		{
+			wait(NULL);
+			return (0);
+		}
+	}
+	return (1);
 }
