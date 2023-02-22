12ec27ae57bcfbba5c126fa9b91e45c83961c434
TriremeSolutions <themellowread@gmail.com>
2023-02-21 19:42:15 -0800

    .

--- entry.c
+++ entry.c
@@ -8,10 +8,10 @@
 void entry(char **argv, char *lineBufPtr)
 {
 	if (strcmp(lineBufPtr, "exit") == 0)
-		{
+	{
 			printf(":-} Madame $helby says Ciao!\n");
 			argv[0] = "exit";
-		}
+	}
 	if (strcmp(argv[0], "hello") == 0)
 		printf("This is Shelby\n");
 }
