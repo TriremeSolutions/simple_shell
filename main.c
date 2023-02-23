260899288ee0982ced6f09568f5fe8470b9da148
TriremeSolutions <themellowread@gmail.com>
2023-02-22 20:09:43 -0800

    commit revert

--- main.c
+++ main.c
@@ -1,45 +1,45 @@
-#include "main.h"
+#include "shell.h"
+
 /**
- * main - shell function
- * @argv: char pointer
- * @argc: integer
- * @envp: environ properties array
- * Return: 0;
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv, char *envp[])
+int main(int ac, char **av)
 {
-	size_t ln = 2;
-	ssize_t chrCount;
-	char *duplicate, *lineBufPtr;
-	const char *delimiter = " \n";
-	char *shellprompt = "\n(0-0) Madame $helby: ";
-	(void)argc;
-	lineBufPtr = NULL;
-	while (1)
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		printf("%s", shellprompt);
-		chrCount = getline(&lineBufPtr, &ln, stdin);
-		if (chrCount == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			printf(":-} Madame $helby will see you later... Goodbye.\n");
-			return (-1);
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		duplicate = malloc(sizeof(char) * chrCount);
-		if (duplicate == NULL)
-		{
-			perror("shelby: memory allocation error");
-			return (-1);
-		} /*user inputs valid, proceed to duplicate*/
-		chops(duplicate, lineBufPtr, delimiter);
-		argv = malloc(sizeof(char *) * ln);
-		argPackets(argv, duplicate, delimiter);
-		env(argv, envp);
-		entry(argv, lineBufPtr);
-		if (strcmp(argv[0], "exit") == 0)
-		return (0);
-		command(argv);
+		info->readfd = fd;
 	}
-	free(lineBufPtr);
-	free(duplicate);
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
+
